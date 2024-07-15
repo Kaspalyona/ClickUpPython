84a6bd0bce83518bd6c01da34dfd62545c79fa43
Alyona <33727197+Kaspalyona@users.noreply.github.com>
2024-07-15 13:03:16 +0300

    Update session.py

--- helpers/session.py
+++ helpers/session.py
@@ -5,12 +5,12 @@ load_dotenv()
 class Session(requests.Session):
     def __init__(self, base_url=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.base_url = "https://api.clickup.com/api/v2"
+        self.base_url = base_url
 
     def request(self, method, url, *args, **kwargs):
         if self.base_url:
             url = self.base_url + url
         headers = kwargs.get('headers', {})
-        headers['Authorization'] = "pk_74545570_U9HXYVA5CWVZSOHLEDWJCV0IQFKEECEG"
+        headers['Authorization'] = os.getenv('TOKEN')
         kwargs['headers'] = headers
         return super().request(method, url, *args, **kwargs)
